231567ed0731df843b1cab7fc38deeee39bedbfc
gabrielblasina <5722@holbertonshool.com>
2023-01-12 10:46:38 -0800

    task-12 output correction

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -1,16 +1,14 @@
 #include "binary_trees.h"
 /**
- * binary_trees_leaves - functionto count the number of leaves
+ * binary_tree_leaves - functionto count the number of leaves
  * Return: Number of leaves (if success) 0 (if fails)
  * @tree: Pointer to given node
 */
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
-	if (!tree)
+	if (tree == NULL)
 		return (0);
-	if (tree->left && tree->right)
-		return (2);
-	else
+	if ((!tree->left) && (!tree->right))
 		return (1);
-
+	return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
 }
