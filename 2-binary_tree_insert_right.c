37249a677b79bf9e3df595986b96bd861023ffdf
binbashz <5732@holbertonstudents.com>
2023-05-15 09:43:58 -0700

    error sintaxys

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -16,7 +16,7 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 	if (parent->right != NULL)
 	{
 		parent->right->parent = newNode;
-		new_node->right = parent->right;
+		newNode->right = parent->right;
 	}
 	parent->right = newNode;
 
