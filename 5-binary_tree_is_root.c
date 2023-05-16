ffe39ca1ca0ff1ca7ced1a0a232a9ea5d9277d56
binbashz <5732@holbertonstudents.com>
2023-05-16 09:11:54 -0700

    tree is root

---
+++ 5-binary_tree_is_root.c
@@ -0,0 +1,12 @@
+#include "binary_trees.h"
+
+	int binary_tree_is_root(const binary_tree_t *node)
+{
+	if (node = NULL)
+	{
+		retunr (0); /*if node is NUll, it cannot be a root */
+
+	}
+
+	return (node);
+}
