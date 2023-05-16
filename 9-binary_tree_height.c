57c2b01f6406e178d34f0d63e09834adb123737d
binbashz <5732@holbertonstudents.com>
2023-05-16 13:25:21 -0700

    error

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -2,27 +2,20 @@
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	if (tree == NULL)
-	{
+    if (tree == NULL)
+    {
+        return (0);  /* If tree is NULL, return height 0 */
+    }
 
-	return (0);  /* If tree is NULL, return height 0 */
-	}
+    size_t left_height = binary_tree_height(tree->left);   /* Calculate the height of the left subtree */
+    size_t right_height = binary_tree_height(tree->right); /* Calculate the height of the right subtree */
 
-	size_t left_height = binary_tree_height(tree->left);
-	/* Calculate the height of the left subtree */
-	size_t right_height = binary_tree_height(tree->right);
-
-	if (left_height > right_height)
-{
-	return (left_height + 1);
-	/* Return the maximum height between the left and right subtrees,*/
-	/* plus 1 for the current node */
-}
-	else
-	{
-	return (right_height + 1);
-	}
-}
-
-
+    if (left_height > right_height)
+    {
+        return (left_height + 1);  /* Return the maximum height between the left and right subtrees, plus 1 for the current node */
+    }
+    else
+    {
+        return (right_height + 1); 
+    }
 }
