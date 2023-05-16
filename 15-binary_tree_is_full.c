2c49e8bc269dc67b2b0117096d157767c09bbfbc
binbashz <5732@holbertonstudents.com>
2023-05-16 15:45:31 -0700

    git add file s

---
+++ 15-binary_tree_is_full.c
@@ -0,0 +1,10 @@
+#include "binary_trees.h"
+
+int binary_tree_is_full(const binary_tree_t *tree);
+{
+	if (tree == NULL)
+	return (0);
+	if (tree->right == NULL && tree->left == NULL)
+
+		return (1);
+}
