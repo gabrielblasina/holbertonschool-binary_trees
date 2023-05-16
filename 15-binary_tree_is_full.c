a8837254b71259e1629b69c385ac9615d4e02f75
binbashz <5732@holbertonstudents.com>
2023-05-16 15:53:35 -0700

    fix

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -8,7 +8,7 @@
 	if (tree->left == NULL && tree->right == NULL)
 	return (1);
 
-    if (tree->left != NULL && tree->right !== NULL)
+    if (tree->left != NULL && tree->right != NULL)
         return (binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right));
 
     return (0);
