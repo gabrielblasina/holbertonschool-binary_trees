ff20ab9ef2cb8cb20e544b8b7823f2cc3f0e2348
binbashz <5732@holbertonstudents.com>
2023-05-16 12:18:24 -0700

    revusrively order ok

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -4,13 +4,15 @@ void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
 	if (tree == NULL || func == NULL)
 	{
-	return;
+	return; /* if tree of func is NULL, o nothing */
 	}
 
 
 
 	func(tree->n);  /* Call the function for the current node of struc */
 
+	/* recurisvely traverse the left subtree*/
 	binary_tree_preorder(tree->left,func);
-	binary_tree_preorder(tree->tight,func); 
+	/* recurisvely tracerse the rigth subtree */
+	binary_tree_preorder(tree->right,func); 
 }
