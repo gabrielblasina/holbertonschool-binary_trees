7c167c41344d9cf5ca4428e025df073c89e1142d
binbashz <5732@holbertonstudents.com>
2023-05-16 09:31:39 -0700

    add ((

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -1,3 +1,11 @@
+
+/**
+ *binary_tree_is_root - checks if a node is root
+ *@node: node to check the root conditon
+ *Return: 1 if root, 0 else
+ */
+
+
 #include "binary_trees.h"
 
 	int binary_tree_is_root(const binary_tree_t *node)
@@ -8,5 +16,6 @@
 
 	}
 
-	return (node->parent == NULL) ? 1 : 0; /* return 1 if node's parent is NULL, otherwise 0 */
+	return ((node->parent == NULL) ? 1 : 0);
+	/* return 1 if node's parent is NULL, otherwise 0 */
 }
