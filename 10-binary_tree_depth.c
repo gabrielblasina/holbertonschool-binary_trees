c1086f0c3a879d2e8f61212387e70c674bc3458c
binbashz <5732@holbertonstudents.com>
2023-05-16 16:15:48 -0700

    if

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -1,17 +1,21 @@
 #include "binary_trees.h"
 
 
+
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
 	size_t deep = 0;
+	const binary_tree_t *current;
 
-	if (tree == NULL)
-	return (0);
+	if (!tree)
+		return (0);
 
-	while (tree->parent = NULL)
+	current = tree;
+
+	while (current->parent)
 	{
-	deep++;
-	tree = tree->parent;
+		deep++;
+		current = current->parent;
 	}
 
 	return (deep);
