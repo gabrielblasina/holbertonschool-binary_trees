2e665de718be2614c51be2508d62be8abf22cfad
binbashz <5732@holbertonstudents.com>
2023-05-16 13:19:39 -0700

    tree_height

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -6,6 +6,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 
 	return (0);  /* If tree is NULL, return height 0 */
 	}
+
 	size_t left_height = binary_tree_height(tree->left);
 	/* Calculate the height of the left subtree */
 	size_t right_height = binary_tree_height(tree->right);
@@ -18,7 +19,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 }
 	else
 	{
-	return (right_height);
+	return (right_height + 1);
 	}
 }
 
