05a9394d82d4bef4c352f1efee687981ab972ac5
binbashz <5732@holbertonstudents.com>
2023-05-16 12:12:01 -0700

    pass value to func

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -2,8 +2,15 @@
 
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
-	if (tree = NULL || func == NULL)
+	if (tree == NULL || func == NULL)
 	{
 	return;
 	}
 
+
+
+	func(tree->n);  /* Call the function for the current node of struc */
+
+	binary_tree_preorder(tree->right);
+	binary_tree_preorder(tree->left); 
+}
